cbe6d0ec79c2eb9cc8cf54ad715f684d924ba604
charles <charles.tecnologiaa@gmail.com>
2025-08-07 12:03:11 -0300

    dia 2 do curso

---
+++ dia2.py
@@ -0,0 +1,49 @@
+
+#Variáveis
+idade = 32
+nome = "Charles"
+altura = 1.75
+
+#Tipos de dados
+#int, float, textos = strings, bool = true or false
+
+#float
+altura = 1.75
+
+# string 
+cidade = 'Recife'
+
+#bool
+esta_logado = True
+
+#int
+idade = 32
+
+#verificar o tipo de dado
+print (type(nome))
+print (type(cidade))
+print (type(esta_logado))
+print (type(idade))
+print (type(altura))
+
+#Operadores
+print (5 + 10)
+print (5 * 10)
+print (10 / 5)
+print (10 - 5)
+
+#Concatenação => União dos textos
+#Usa-se o símbolo '+'
+
+nomeUsuario = "Charles"
+frase = "olá " + nomeUsuario + " Tudo bem? você mora em " + cidade
+
+print(frase)
+
+#Comparações
+
+maior = 10 > 5
+igual = 10 == 10
+diferente = 5 != 10
+
+print(maior, igual, diferente)
